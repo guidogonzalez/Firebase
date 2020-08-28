d516098968ca9a7ea28c38e78f5bcb7a6f1fd602
Wido <ggonzalez171292@gmail.com>
2020-08-28 13:34:26 +0200

    Añadida conexión a la bdd en el repo y pasado a viewmodel

--- app/src/main/java/es/widoapps/firebase/repositorio/Repositorio.java
+++ app/src/main/java/es/widoapps/firebase/repositorio/Repositorio.java
@@ -1,4 +1,58 @@
 package es.widoapps.firebase.repositorio;
 
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.widoapps.firebase.modelo.Personaje;
+
 public class Repositorio {
+
+    public static Repositorio instancia;
+    private List<Personaje> listaPersonajes = new ArrayList<>();
+    private MutableLiveData<List<Personaje>> personajes = new MutableLiveData<>();
+    private FirebaseFirestore bd = FirebaseFirestore.getInstance();
+
+    public static Repositorio getInstance() {
+
+        if (instancia == null) {
+            instancia = new Repositorio();
+        }
+
+        return instancia;
+    }
+
+    public MutableLiveData<List<Personaje>> getPersonajes() {
+
+        bd.collection("dragonball")
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+
+                    if (!queryDocumentSnapshots.isEmpty()) {
+
+                        List<DocumentSnapshot> lista = queryDocumentSnapshots.getDocuments();
+
+                        for (DocumentSnapshot documentSnapshot : lista) {
+
+                            Personaje personaje = documentSnapshot.toObject(Personaje.class);
+                            personaje.setId(documentSnapshot.getId());
+
+                            Log.d("PERSONAJE_REMOTO", personaje.nombre);
+
+                            listaPersonajes.add(personaje);
+                        }
+
+                        personajes.setValue(listaPersonajes);
+                    }
+
+                });
+
+        return personajes;
+    }
 }
--- app/src/main/java/es/widoapps/firebase/viewmodel/ListaViewModel.java
+++ app/src/main/java/es/widoapps/firebase/viewmodel/ListaViewModel.java
@@ -1,4 +1,26 @@
 package es.widoapps.firebase.viewmodel;
 
-public class ListaViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import es.widoapps.firebase.modelo.Personaje;
+import es.widoapps.firebase.repositorio.Repositorio;
+
+public class ListaViewModel extends AndroidViewModel {
+
+    private MutableLiveData<List<Personaje>> listaPersonajes = new MutableLiveData<>();
+
+    public ListaViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void cargarPersonajesRemoto() {
+
+        listaPersonajes = Repositorio.getInstance().getPersonajes();
+    }
 }
